6d0a229ccdd753ba4e0942a13f2665bb2f484524
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-09-05 00:26:21 -0300

    Setup Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBiFGEs69-NvDNfVbZY2YpTPtv-dvKayGI",
+      authDomain: "reactnative-auth-e453f.firebaseapp.com",
+      databaseURL: "https://reactnative-auth-e453f.firebaseio.com",
+      projectId: "reactnative-auth-e453f",
+      storageBucket: "reactnative-auth-e453f.appspot.com",
+      messagingSenderId: "491177441040"
+    });
+  }
+
   render() {
     return (
       <View>
