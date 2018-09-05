8e0795d271e16b8b760e19cacd5bd99417e6c10d
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-09-05 02:07:40 -0300

    Create error handling for failed login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
   };
 
+  onButtonPress = () => {
+    const { email, password } = this.state;
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+          this.setState({ error: 'Authentication failed' });
+        })
+      })
+  }
+
   render() {
+    const { email, password, error } = this.state;
+    const { errorTextStyle } = styles;
+
     return (
       <Card>
 
@@ -15,7 +37,7 @@ class LoginForm extends Component {
           <Input
             label="Email"
             placeholder="user@email.com"
-            value={this.state.email}
+            value={email}
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
@@ -25,13 +47,15 @@ class LoginForm extends Component {
             secureTextEntry
             label="Password"
             placeholder="password"
-            value={this.state.password}
+            value={password}
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
 
+        <Text style={errorTextStyle}>{error}</Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress}>
             Login
           </Button>
         </CardSection>
@@ -40,5 +64,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
