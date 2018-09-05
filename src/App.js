2abc9273c5dacbebaa90faba601e0ae3a7edc255
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-09-05 02:58:48 -0300

    Setup conditional rendering on app load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyBiFGEs69-NvDNfVbZY2YpTPtv-dvKayGI",
@@ -14,13 +18,38 @@ class App extends Component {
       storageBucket: "reactnative-auth-e453f.appspot.com",
       messagingSenderId: "491177441040"
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent = () => {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={null}>
+            Log Out
+          </Button>
+        );
+        break;
+      case false:
+        return  <LoginForm />;
+        break;
+      default:
+        return  <Spinner />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
